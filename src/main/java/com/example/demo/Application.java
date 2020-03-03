00a4ae780ef9bf555e92ec5f25d3daf10aaa4589
cajgomez <Jgomez@cambridgeassociates.com>
2020-03-03 13:07:22 -0600

    Adding a Pubsub template to read from the subscription.

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Bean;
 
 @EnableBinding(Channels.class)
 @SpringBootApplication
@@ -10,6 +13,18 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner cli(PubSubTemplate pubSubTemplate) {
+		return (args) -> {
+			pubSubTemplate.subscribe("reservations",
+					(msg, ackConsumer) -> {
+						System.out.println(msg.getData().toStringUtf8());
+						ackConsumer.ack();
+					});
+		};
 	}
 
 }
